a3ce39049a865d2aa0a4366184663e71fd9dc3e8
makotowzhang <31465825+makotowzhang@users.noreply.github.com>
2018-09-06 09:56:54 +0800

    Base

---
+++ Model/SystemModel/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Model.SystemModel
+{
+    public class UserModel
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string TrueName { get; set; }
+        public bool? IsEnabled { get; set; } = true;
+        public bool? IsDel { get; set; } = false;
+        public List<string> UserRole { get; set; }
+        public string CreateUser { get; set; }
+        public DateTime? CreateTime { get; set; }
+        public string UpdateUser { get; set; }
+        public DateTime? UpdateTime { get; set; }
+    }
+}
